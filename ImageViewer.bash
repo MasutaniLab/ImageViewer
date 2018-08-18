9ee530b13462567e1fca95d6eb690028f1624904
MASUTANI Yasuhiro <masutani@osakac.ac.jp>
2018-08-18 21:16:05 +0900

    起動用シェルスクリプトを微修正

--- ImageViewer.bash
+++ ImageViewer.bash
@@ -7,8 +7,8 @@ name=${com}
 #「README.モジュール名」というファイルがあることが前提．
 case "$1" in
 0|1|2|3|4|5|6|7|8|9)
-    cat=$(sed -ne '/^Category:/s/.*: *//p' README.${com})
-    mod=$(sed -ne '/^Module Name:/s/.*: *//p' README.${com})
+    cat=$(sed -ne '/^Category:/s/.*: *//p' ${dir}/README.${com})
+    mod=$(sed -ne '/^Module Name:/s/.*: *//p' ${dir}/README.${com})
     name=${com}$1
     opt="-o ${cat}.${mod}.instance_name:${name}"
     shift
