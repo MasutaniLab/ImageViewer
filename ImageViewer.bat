95b49bcb109926eee63ce3c7192dc6edbbe2716b
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2018-09-16 16:02:22 +0900

    WindowsでQRコードを読めるようにした．https://github.com/dani4/ZBarWin64 を利用．

--- ImageViewer.bat
+++ ImageViewer.bat
@@ -1,5 +1,6 @@
 @echo off
 pushd %~dp0
+set Path=..\ZBarWin64\lib;..\ZBarWin64\zbar\libiconv\dll_x64;%Path%
 if not defined RTM_VC_CONFIG set RTM_VC_CONFIG=Release
 start "%~n0" "build\src\%RTM_VC_CONFIG%\%~n0Comp.exe"
 popd
