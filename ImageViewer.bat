32ad6926820793f202268c986a442e863a6b5e5b
y-masutani <masutani@isc.osakac.ac.jp>
2018-09-22 21:58:08 +0900

    起動用バッチファイルが引数を取るようにした．

--- ImageViewer.bat
+++ ImageViewer.bat
@@ -2,5 +2,5 @@
 pushd %~dp0
 set Path=..\ZBarWin64\lib;..\ZBarWin64\zbar\libiconv\dll_x64;%Path%
 if not defined RTM_VC_CONFIG set RTM_VC_CONFIG=Release
-start "%~n0" "build\src\%RTM_VC_CONFIG%\%~n0Comp.exe"
+start "%~n0 %1 %2 %3 %4 %5 %6 %7 %8 %9" "build\src\%RTM_VC_CONFIG%\%~n0Comp.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
 popd
